e7fdf0f56b2019cf631406b3483479ca3599cb68
LUCHER4321 <lucher4321@gmail.com>
2025-05-03 23:02:17 -0400

    Create config.js

---
+++ build/config.js
@@ -0,0 +1,5 @@
+"use strict";
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PORT = void 0;
+exports.PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
